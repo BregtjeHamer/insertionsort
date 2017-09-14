6188667175d129626b49dd9149cb7c347470bb57
BregtjeHamer <bregtjehamer@hotmail.com>
2017-09-14 18:53:41 +0200

    hij sorteert de 2 rijen apart.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,56 +8,67 @@ public class Main {
 //
         int aantal = 10;
 
+        int lijst1[] = new int[aantal / 2];
+        int lijst2[] = new int[aantal / 2];
 
         for (int i = 0; i < 10; i++) {
 
 //            int ongesorteerd[] = new int[aantal];
 
-            int lijst1[] = new int[aantal / 2];
-            int lijst2[] = new int[aantal / 2];
 
             for (int j = 0; j < lijst1.length; j++) {
                 lijst1[j] = (int) (Math.random() * aantal) + 1;
                 lijst2[j] = (int) (Math.random() * aantal) + 1;
             }
+        }
+
+        new Main().print(lijst1);
+        System.out.println("\n");
+        new Main().print(lijst2);
+        System.out.println("\n");
+
+        SortThread s1 = new SortThread(lijst1);
+        SortThread s2 = new SortThread(lijst2);
+
+        Thread t1 = new Thread(s1);
+        Thread t2 = new Thread(s2);
+
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+
+        int[] gesorteerd1 = s1.getTeSorteren();
+        new Main().print(gesorteerd1);
+        System.out.println("\n");
+
+        int[] gesorteerd2 = s2.getTeSorteren();
+        new Main().print(gesorteerd2);
+        System.out.println("\n");
+
+        
 
 
-            SortThread s1 = new SortThread(lijst1);
-            SortThread s2 = new SortThread(lijst2);
-
-            Thread t1 = new Thread(s1);
-            Thread t2 = new Thread(s2);
-
-            t1.start();
-            t2.start();
-            try {
-                t1.join();
-                t2.join();
-            } catch (InterruptedException ex) {
-                System.out.println(ex);
-            }
-
-            int[] gesorteerd1 = s1.getTeSorteren();
-            int[] ongesorteerd2 = s2.getTeSorteren();
-
-            //sorteren
+        //sorteren
 //            new Main().print(ongesorteerd);
 //            System.out.println("\n");
 
 
-            System.out.println("\n");
-            long startTime = System.nanoTime();
-//            Sort.sorteren(ongesorteerd);
-
-            long endTime = System.nanoTime();
-//            new Main().print(ongesorteerd);
-
-            long duration = endTime - startTime;
-            System.out.println("\n");
-            System.out.println("looptijd " + (i + 1) + ": " + duration / 1000000 + "ms");
-//            totaal[i] = (int) (duration / 1000000);
-        }
-
+//            System.out.println("\n");
+//            long startTime = System.nanoTime();
+////            Sort.sorteren(ongesorteerd);
+//
+//            long endTime = System.nanoTime();
+////            new Main().print(ongesorteerd);
+//
+//            long duration = endTime - startTime;
+//            System.out.println("\n");
+//            System.out.println("looptijd " + (i + 1) + ": " + duration / 1000000 + "ms");
+////            totaal[i] = (int) (duration / 1000000);
     }
 
 
